72522754968e4a1fd05b17eed78332db4a07ec91
Artem Safin <artemsafin.webdev@gmail.com>
2022-05-25 19:46:46 +0300

    Правки

--- src/js/numbersPeopleSlider.js
+++ src/js/numbersPeopleSlider.js
@@ -15,9 +15,9 @@ export default function numbersPeopleSlider() {
             fadeEffect: {
                 crossFade: true
             },
-            // autoplay: {
-            //     delay: 1800
-            // }
+            autoplay: {
+                delay: 1800
+            }
         });
     });
 }
